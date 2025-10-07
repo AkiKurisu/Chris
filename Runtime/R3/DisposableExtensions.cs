1cab19af64a971cc2e5de250ebd3b8ffb355ab53
AkiKurisu <2683987717@qq.com>
2025-10-07 15:30:23 +0800

    Remove DisposableUnregister

--- Runtime/R3/DisposableExtensions.cs
+++ Runtime/R3/DisposableExtensions.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Threading;
-using Chris;
-using R3.Triggers;
-using UnityEngine;
+
 namespace R3.Chris
 {
     /// <summary>
@@ -16,67 +13,10 @@ namespace R3.Chris
         /// <param name="disposable"></param>
         void Register(IDisposable disposable);
     }
-    
-    public readonly struct DisposableUnregister
-    {
-        private readonly ObservableDestroyTrigger _trigger;
-        
-        private readonly CancellationToken _cancellationToken;
-        
-        public DisposableUnregister(ObservableDestroyTrigger trigger)
-        {
-            _trigger = trigger;
-            _cancellationToken = default;
-        }
-        
-        public DisposableUnregister(CancellationToken cancellationToken)
-        {
-            _trigger = null;
-            _cancellationToken = cancellationToken;
-        }
-        
-        public void Register(IDisposable disposable)
-        {
-            if (_trigger)
-            {
-                _trigger.AddDisposableOnDestroy(disposable);
-            }
-            else
-            {
-                disposable.RegisterTo(_cancellationToken);
-            }
-        }
-    }
-    
+
     public static class DisposableExtensions
     {
-        /// <summary>
-        /// Get or create an UnRegister from <see cref="GameObject"/>, listening OnDestroy event
-        /// </summary>
-        /// <param name="gameObject"></param>
-        /// <returns></returns>
-        public static DisposableUnregister GetUnregister(this GameObject gameObject)
-        {
-            return new DisposableUnregister(gameObject.GetOrAddComponent<ObservableDestroyTrigger>());
-        }
-        
-        /// <summary>
-        ///  Get or create an UnRegister from <see cref="MonoBehaviour"/>, listening OnDestroy event
-        /// </summary>
-        /// <param name="monoBehaviour"></param>
-        /// <returns></returns>
-        public static DisposableUnregister GetUnregister(this MonoBehaviour monoBehaviour)
-        {
-            return new DisposableUnregister(monoBehaviour.destroyCancellationToken);
-        }
-        
         public static T AddTo<T>(this T disposable, IDisposableUnregister unRegister) where T : IDisposable
-        {
-            unRegister.Register(disposable);
-            return disposable;
-        }
-        
-        public static T AddTo<T>(this T disposable, DisposableUnregister unRegister) where T : IDisposable
         {
             unRegister.Register(disposable);
             return disposable;
--- Runtime/R3/ObservableExtensions.cs
+++ Runtime/R3/ObservableExtensions.cs
@@ -98,44 +98,5 @@ namespace R3.Chris
             property.Subscribe(toggle.SetIsOnWithoutNotify).AddTo(unRegister);
             toggle.SetIsOnWithoutNotify(property.Value);
         }
-        
-        /// <summary>
-        /// Bind <see cref="ReactiveProperty{Single}"/> to slider
-        /// </summary>
-        /// <param name="slider"></param>
-        /// <param name="property"></param>
-        /// <param name="unRegister"></param>
-        public static void BindProperty(this Slider slider, ReactiveProperty<float> property, DisposableUnregister unRegister) 
-        {
-            slider.onValueChanged.AsObservable().Subscribe(e => property.Value = e).AddTo(unRegister);
-            property.Subscribe(slider.SetValueWithoutNotify).AddTo(unRegister);
-            slider.SetValueWithoutNotify(property.Value);
-        }
-        
-        /// <summary>
-        /// Bind <see cref="ReactiveProperty{Int32}"/> to slider
-        /// </summary>
-        /// <param name="slider"></param>
-        /// <param name="property"></param>
-        /// <param name="unRegister"></param>
-        public static void BindProperty(this Slider slider, ReactiveProperty<int> property, DisposableUnregister unRegister)
-        {
-            slider.onValueChanged.AsObservable().Subscribe(e => property.Value = (int)e).AddTo(unRegister);
-            property.Subscribe(e => slider.SetValueWithoutNotify(e)).AddTo(unRegister);
-            slider.SetValueWithoutNotify(property.Value);
-        }
-        
-        /// <summary>
-        /// Bind <see cref="ReactiveProperty{Boolean}"/> to toggle
-        /// </summary>
-        /// <param name="toggle"></param>
-        /// <param name="property"></param>
-        /// <param name="unRegister"></param>
-        public static void BindProperty(this Toggle toggle, ReactiveProperty<bool> property, DisposableUnregister unRegister)
-        {
-            toggle.onValueChanged.AsObservable().Subscribe(e => property.Value = e).AddTo(unRegister);
-            property.Subscribe(toggle.SetIsOnWithoutNotify).AddTo(unRegister);
-            toggle.SetIsOnWithoutNotify(property.Value);
-        }
     }
 }
