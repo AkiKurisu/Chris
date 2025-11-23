7c94a67ae3071a767c86f9222be7bf1249741441
AkiKurisu <2683987717@qq.com>
2025-11-23 23:00:08 +0800

    Add UGUIExtensions

--- Runtime/R3/ObservableExtensions.cs
+++ Runtime/R3/ObservableExtensions.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading;
 using Chris;
 using Chris.Events;
-using UnityEngine.UI;
+
 namespace R3.Chris
 {
     public static class ObservableExtensions
@@ -58,45 +58,6 @@ namespace R3.Chris
                 onNext(evt);
                 evt.Dispose();
             }
-        }
-        
-        /// <summary>
-        /// Bind <see cref="ReactiveProperty{Single}"/> to slider
-        /// </summary>
-        /// <param name="slider"></param>
-        /// <param name="property"></param>
-        /// <param name="unRegister"></param>
-        public static void BindProperty(this Slider slider, ReactiveProperty<float> property, IDisposableUnregister unRegister)
-        {
-            slider.onValueChanged.AsObservable().Subscribe(e => property.Value = e).AddTo(unRegister);
-            property.Subscribe(slider.SetValueWithoutNotify).AddTo(unRegister);
-            slider.SetValueWithoutNotify(property.Value);
-        }
-        
-        /// <summary>
-        /// Bind <see cref="ReactiveProperty{Int32}"/> to slider
-        /// </summary>
-        /// <param name="slider"></param>
-        /// <param name="property"></param>
-        /// <param name="unRegister"></param>
-        public static void BindProperty(this Slider slider, ReactiveProperty<int> property, IDisposableUnregister unRegister)
-        {
-            slider.onValueChanged.AsObservable().Subscribe(e => property.Value = (int)e).AddTo(unRegister);
-            property.Subscribe(e => slider.SetValueWithoutNotify(e)).AddTo(unRegister);
-            slider.SetValueWithoutNotify(property.Value);
-        }
-        
-        /// <summary>
-        /// Bind <see cref="ReactiveProperty{Boolean}"/> to toggle
-        /// </summary>
-        /// <param name="toggle"></param>
-        /// <param name="property"></param>
-        /// <param name="unRegister"></param>
-        public static void BindProperty(this Toggle toggle, ReactiveProperty<bool> property, IDisposableUnregister unRegister)
-        {
-            toggle.onValueChanged.AsObservable().Subscribe(e => property.Value = e).AddTo(unRegister);
-            property.Subscribe(toggle.SetIsOnWithoutNotify).AddTo(unRegister);
-            toggle.SetIsOnWithoutNotify(property.Value);
         }
     }
 }
---
+++ Runtime/R3/UGUIExtensions.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.UI;
+
+namespace R3.Chris
+{
+    public static class ReactivePropertyExtensions
+    {
+         
+        /// <summary>
+        /// Bind <see cref="ReactiveProperty{Single}"/> to slider
+        /// </summary>
+        /// <param name="slider"></param>
+        /// <param name="property"></param>
+        /// <param name="unRegister"></param>
+        public static void BindProperty(this Slider slider, ReactiveProperty<float> property, IDisposableUnregister unRegister)
+        {
+            slider.onValueChanged.AsObservable().Subscribe(e => property.Value = e).AddTo(unRegister);
+            property.Subscribe(slider.SetValueWithoutNotify).AddTo(unRegister);
+            slider.SetValueWithoutNotify(property.Value);
+        }
+        
+        /// <summary>
+        /// Bind <see cref="ReactiveProperty{Int32}"/> to slider
+        /// </summary>
+        /// <param name="slider"></param>
+        /// <param name="property"></param>
+        /// <param name="unRegister"></param>
+        public static void BindProperty(this Slider slider, ReactiveProperty<int> property, IDisposableUnregister unRegister)
+        {
+            slider.onValueChanged.AsObservable().Subscribe(e => property.Value = (int)e).AddTo(unRegister);
+            property.Subscribe(e => slider.SetValueWithoutNotify(e)).AddTo(unRegister);
+            slider.SetValueWithoutNotify(property.Value);
+        }
+        
+        /// <summary>
+        /// Bind <see cref="ReactiveProperty{Boolean}"/> to toggle
+        /// </summary>
+        /// <param name="toggle"></param>
+        /// <param name="property"></param>
+        /// <param name="unRegister"></param>
+        public static void BindProperty(this Toggle toggle, ReactiveProperty<bool> property, IDisposableUnregister unRegister)
+        {
+            toggle.onValueChanged.AsObservable().Subscribe(e => property.Value = e).AddTo(unRegister);
+            property.Subscribe(toggle.SetIsOnWithoutNotify).AddTo(unRegister);
+            toggle.SetIsOnWithoutNotify(property.Value);
+        }
+    }
+}
