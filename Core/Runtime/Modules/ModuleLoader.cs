32ca6133246ad16057916a1cc75f5ba54d3d2978
AkiKurisu <2683987717@qq.com>
2026-03-01 21:23:30 +0800

    Add explicit runtime module config and editor UI

--- Core/Runtime/Modules/ModuleLoader.cs
+++ Core/Runtime/Modules/ModuleLoader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using Chris.Configs;
 using UnityEngine;
@@ -11,20 +11,39 @@ namespace Chris.Modules
         private static void InitializeModules()
         {
             ConfigsModule.InitializeInternal();
-            var modules = AppDomain.CurrentDomain.GetAssemblies()
-    #if UNITY_EDITOR
-                    .Where(assembly => !assembly.GetName().Name.Contains(".Editor"))
-#endif
-                    .SelectMany(assembly => assembly.GetTypes())
-                    .Where(type => typeof(RuntimeModule).IsAssignableFrom(type) && !type.IsAbstract)
-                    .Select(type => (RuntimeModule)Activator.CreateInstance(type))
-                    .OrderBy(module => module.Order)
-                    .ToArray();
-
+            
+            var config = ModuleConfig.Get();
+            var modules = config.Modules.Length > 0
+                ? GetModulesFromConfig(config)
+                : GetModulesFromAssemblies();
+            
             foreach (var module in modules)
             {
                 module.Initialize();
             }
+        }
+
+        private static RuntimeModule[] GetModulesFromConfig(ModuleConfig config)
+        {
+            return config.Modules
+                .Select(st => st.GetObjectType())
+                .Where(type => type != null)
+                .Select(type => (RuntimeModule)Activator.CreateInstance(type))
+                .OrderBy(module => module.Order)
+                .ToArray();
+        }
+
+        private static RuntimeModule[] GetModulesFromAssemblies()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+#if UNITY_EDITOR
+                .Where(assembly => !assembly.GetName().Name.Contains(".Editor"))
+#endif
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => typeof(RuntimeModule).IsAssignableFrom(type) && !type.IsAbstract)
+                .Select(type => (RuntimeModule)Activator.CreateInstance(type))
+                .OrderBy(module => module.Order)
+                .ToArray();
         }
     }
 }
